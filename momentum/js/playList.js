0c09da7aaf82d3737322d8843a0630517fc42919
AntonLeshkovich <77743070+AntonLeshkovich@users.noreply.github.com>
2023-10-17 22:07:38 +0300

    feat: move momentum from private repository

---
+++ momentum/js/playList.js
@@ -0,0 +1,24 @@
+const playList = [
+    {
+        title: 'Imagine Dragons - Wrecked',
+        src: './assets/sounds/Imagine_Dragons_Wrecked.mp3',
+        duration: '04:04',
+    },
+    {
+        title: 'Pitbull feat. Kesha - Timber',
+        src: './assets/sounds/Pitbull_feat_Kesha-Timber.mp3',
+        duration: '03:24',
+    },
+    {
+        title: 'Red Hot Chili Peppers - Otherside',
+        src: './assets/sounds/RedHotChiliPeppers-Otherside.mp3',
+        duration: '04:18',
+    },
+    {
+        title: 'Nicole en Hugo - Goeiemorgen morgen ',
+        src: './assets/sounds/Nicole_en_Hugo-Goeiemorgen_morgen.mp3',
+        duration: '02:49',
+    }
+]
+
+export default playList;
